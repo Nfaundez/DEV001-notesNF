e37f38a18911ef0fef00f47b0bc3ebe49544cf20
giolujan <giolujan123@gmil.com>
2023-02-20 11:33:13 -0500

    borrador

--- labnotes/src/routes/root.jsx
+++ labnotes/src/routes/root.jsx
@@ -2,39 +2,41 @@ import { useNavigate } from "react-router-dom";
 import { loginWithGoogle } from "../firebase/firebaseConfig";
 import Header from "../components/header";
 
-export default function Root () {
+export default function Root() {
   const navigate = useNavigate();
 
   const signInWithGoogle = () => {
-      loginWithGoogle().then((res) => {
-          const displayNameUser = res.user.displayName;
-          const emailUser = res.user.email;
-          //actualiza un valor si la clave ya existe
-          localStorage.setItem("name", displayNameUser);
-          localStorage.setItem("email", emailUser);
-          navigate("/wall/:wallId");
-        })
-        .catch( console.error )
+    loginWithGoogle().then((res) => {
+      const displayNameUser = res.user.displayName;
+      const emailUser = res.user.email;
+      //actualiza un valor si la clave ya existe
+      localStorage.setItem("name", displayNameUser);
+      localStorage.setItem("email", emailUser);
+      navigate("/wall/:wallId");
+    })
+      .catch(console.error)
   };
 
   return (
     <>
-      <Header/>
-        <div id="sidebar">
+      <Header />
+      <main>
+      <div id="sidebar">
           <h1>Login</h1>
+          <p>¡No olvides dejar tus notas!</p>
           <div>
-            <form method="post">
-                <h2>Ingresa con:</h2>
+            <form>
+              <h2>Ingresa con:</h2>
               <button type="submit" onClick={signInWithGoogle}>Google</button>
             </form>
           </div>
-        </div>
-      </>
-);
+      </div>
+      </main>
+    </>
+  );
 };
 
 
 
-          
-          
-  
+
+
