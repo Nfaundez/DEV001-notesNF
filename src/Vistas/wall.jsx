db8e6f8e79a2d6d53c986b8015d20c9ecde63680
giolujan <giolujan123@gmil.com>
2023-03-01 14:01:18 -0500

    cambios pequeños

--- src/Vistas/wall.jsx
+++ src/Vistas/wall.jsx
@@ -10,7 +10,7 @@ import React, { useEffect, useState } from "react";
 export default function Wall() {
 
   // useState es un Hook que te permite añadir el estado de React a un componente de función.
-  
+
 
 
   const saveNote = async (title, description) => {
@@ -22,23 +22,24 @@ export default function Wall() {
 
   const [listNote, setListNote] = useState([])
 
-// funcion renderizar contenido de nota y titulo
-  useEffect(()=>{
-    const getList = async() => {
-      try {
-        const querySnapshot = await getDocs(collection(db, 'notes'))
-        const docs = [];
-        querySnapshot.forEach((doc)=> {
-          docs.push({...doc.data(), id:doc.id})
-        })
-        setListNote(docs)
-      }catch (error){
-        console.log(error)
-      }
+  const getList = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, 'notes'))
+      const docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id })
+      })
+      setListNote(docs)
+    } catch (error) {
+      console.log(error)
     }
-    getList()
-  },[listNote])
-  
+  }
+
+  // funcion renderizar contenido de nota y titulo
+  useEffect(() => {
+    getList(listNote)
+  }, [])
+
 
   return (
     <div id="wall">
@@ -51,22 +52,23 @@ export default function Wall() {
 
       <div className="notessaved">
         <div className="note-body">
-        {
-          listNote.map(list => (
-          <div className="notita" 
-          key={`div${list.id}`}>
-          <h2 className="title">{list.title}</h2>
-          <p className="description">{list.description}</p>
-          {/* <div className="buttons">
+          {
+            listNote.map(note => (
+              <div className="notita"
+                key={`div${note.id}`}>
+                <h2 className="title">{note.title}</h2>
+                <p className="description">{note.description}</p>
+                <button className="buttonDelete">Eliminar</button>
+          <button className="buttonEdit">Editar</button> 
+                {/* <div className="buttons">
            <button className="buttonDelete">Eliminar</button>
           <button className="buttonEdit">Editar</button> 
           </div> */}
-          
-          </div>
-))
-        }
+              </div>
+            ))
+          }
         </div>
-        
+
       </div>
     </div>
   )
