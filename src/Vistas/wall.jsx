1ab557a455bcbc219a64107bc67f550951932f67
giolujan <giolujan123@gmil.com>
2023-03-03 12:37:25 -0500

    avance del botonr

--- src/Vistas/wall.jsx
+++ src/Vistas/wall.jsx
@@ -3,7 +3,7 @@ import Buttonclose from "../components/buttonclose";
 import Wallform from "./wallForm";
 import './wall.css';
 import { db } from "../firebase/firebaseConfig";
-import { addDoc, collection, deleteDoc, getDocs, doc } from "firebase/firestore";
+import { collection, deleteDoc, getDocs, doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 
 
@@ -11,66 +11,82 @@ export default function Wall() {
 
   // useState es un Hook que te permite añadir el estado de React a un componente de función.
 
-
-
-  const saveNote = async (title, description) => {
-    //el metodo adddoc() agrega un identificador, quiero guardar un objeto documento en la collecion
-    await addDoc(collection(db, 'notes'), title, description);
-    console.log("guardado");
-  }
-
-
+  const [getNoteEdit, setgetNoteEdit] = useState([])
   const [listNote, setListNote] = useState([])
+  const [subId, setSubId] = useState('')
 
-  const getList = async() => {
+  const getList = async () => {
     try {
       const querySnapshot = await getDocs(collection(db, 'notes'))
       const docs = [];
-      querySnapshot.forEach((doc)=> {
-        docs.push({...doc.data(), id:doc.id})
+      querySnapshot.forEach((doc) => {
+        docs.push({ ...doc.data(), id: doc.id })
       })
       setListNote(docs)
-    }catch (error){
+    } catch (error) {
       console.log(error)
     }
-   };
+  };
 
-// funcion renderizar contenido de nota y titulo
-  useEffect(()=>{
+  const getOneNote = async (id) => {
+    try {
+      const docRef = doc(db, 'notes', id)
+      const docSnap = await getDoc(docRef)
+      setgetNoteEdit(docSnap.data())
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    if (subId !== '') {
+      getOneNote(subId)
+    }
+    console.log(getNoteEdit)
+  }, [subId])
+
+  // funcion renderizar contenido de nota y titulo
+  useEffect(() => {
     getList()
-  },[]);
-    
+  }, []);
+
+
+
+
   // funcion para eliminar notas
-  const deleteNote = async (id) =>{
-  await deleteDoc (doc(db, 'notes', id))
-}
+  const deleteNote = async (id) => {
+    await deleteDoc(doc(db, 'notes', id))
+  }
+
 
   return (
     <div id="wall">
       <Header />
       <Buttonclose />
       <div id="wallinwall">
-        <Wallform saveNote={saveNote} />
+         <Wallform  /> 
         <div className="divWall" id="divWall">
-        </div></div>
+        </div>
+      </div>
 
       <div className="notessaved">
         <div className="note-body">
-        {
-          listNote.map(list => (
-          <div className="notita" 
-          key= {`div${list.id}`}>
-          <h2 className="title">{list.title}</h2>
-          <p className="description">{list.description}</p>
-          <div className="buttons">
-           <button className="buttonDelete" onClick={()=>deleteNote(list.id)}>
-            Eliminar
-            </button>
-           <button className="buttonEdit">Editar</button> 
-           </div>
-          </div> 
-))
-        }
+          {
+            listNote.map(list => (
+              <div className="notita"
+                key={`div${list.id}`}>
+                <h2 className="title">{list.title}</h2>
+                <p className="description">{list.description}</p>
+                <div className="buttons">
+                  <button className="buttonDelete" onClick={() => deleteNote(list.id)}>
+                    Eliminar
+                  </button>
+                  <button className="buttonEdit" onClick={() => setSubId(list.id)}>Editar</button>
+                </div>
+              </div>
+
+            ))
+          }
         </div>
 
       </div>
