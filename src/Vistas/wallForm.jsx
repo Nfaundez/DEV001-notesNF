4906ed60458478a42756338faef32003de56b711
giolujan <giolujan123@gmil.com>
2023-03-03 12:40:21 -0500

    avance del boton editar

--- src/Vistas/wallForm.jsx
+++ src/Vistas/wallForm.jsx
@@ -1,23 +1,46 @@
-import React, {useEffect, useState} from "react";
+import React, { useState } from "react";
 import './wall.css';
+import { db } from "../firebase/firebaseConfig";
+import { addDoc, collection } from "firebase/firestore"
+
 export default function Wallform(props) {
+
+  // const editNote = {
+  //  title: props.values.title,
+  //  description: props.values.description
+  // } values={values}
 
   const initialNote = {
     title: '',
     description: ''
   }
-// variables de estado
+
+  // variables de estado
   const [values, setValues] = useState(initialNote)
- 
+  //console.log(props.values)
+  if (props.values !== '') {
+    // console.log(editNote)
+    // console.log(useState(editNote))
+    // setValues(editNote)
+  } else {
+    // console.log(useState(initialNote))
+    setvalues(initialNote)
+  }
+
+  const saveNote = async (title, description) => {
+    //el metodo adddoc() agrega un identificador, quiero guardar un objeto documento en la collecion
+    await addDoc(collection(db, 'notes'), title, description);
+    console.log("guardado");
+  }
 
 
   //actualiza el form
   const handleSubmit = e => {
     e.preventDefault();
-    props.saveNote(values);
+    saveNote(values);
     setValues({ ...initialNote })
   }
-  
+
   //recoge los datos de los imputs
   const handleInputChange = e => {
     e.preventDefault();
@@ -26,18 +49,13 @@ export default function Wallform(props) {
     setValues({ ...values, [name]: value });
   }
 
-
-  const Wallform = {
-
-  };
-
   return (
     <>
       <form id="wallform" onSubmit={handleSubmit}>
-      <label>Titulo:</label>
-        <input type="text" className="noteTitle" name="title" placeholder="titulo" onChange={handleInputChange} value={values.title}></input>
+        <label>Titulo:</label>
+        <input type="text" className="noteTitle" name="title" placeholder="titulo" onChange={handleInputChange} value={values.title} ></input>
         <textarea className="noteDescription" name="description" rows="10" placeholder="deja tu nota" onChange={handleInputChange} value={values.description}></textarea>
-        <button className="buttonSave">Guardar</button>
+        <button className="buttonSave" onClick={saveNote} >Guardar</button>
       </form>
     </>
   );
